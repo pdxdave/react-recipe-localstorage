aa15a100753dbfd6f9ba4135f62b0d8a9872bbdd
pdxdave <webdevdave@hotmail.com>
2021-05-31 14:05:15 -0700

    setting up cards in search list recipe

--- src/pages/SearchListRecipe.js
+++ src/pages/SearchListRecipe.js
@@ -1,11 +1,18 @@
-
+import style from '../styles/searchlistrecipe.module.css'
 
 const SearchListRecipe = ({food}) => {
     
     return (
         <div>
-            <img src={food.recipe.image} alt={food.recipe.label} />
-            <p>{food.recipe.label}</p>
+            <div className={style.recipe_card}>
+                <div className={style.recipe_card_image}>
+                    <img src={food.recipe.image} alt={food.recipe.label} />
+                    <p className={style.card_label}>
+                     {food.recipe.label.length > 20 ? food.recipe.label.substring(0, 25) + '...' : food.recipe.label}   
+                    </p> 
+                </div>
+                <button>Recipe Details</button>
+            </div>
         </div>
     )
 }
