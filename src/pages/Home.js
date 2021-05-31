44f104fe9f16bbbce48558aba4cb01e932200ce1
pdxdave <webdevdave@hotmail.com>
2021-05-30 22:34:22 -0700

    more homepage css and design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,22 @@ import style from '../styles/home.module.css'
 const Home = () => {
     return (
         <div className={style.banner}>
-            
+            <div className={style.overlay}>
+                {/* <div className={style.title}>
+                    <h1>Search from</h1>
+                    <h1>Thousands of </h1>
+                    <h1>Recipes for your</h1>
+                    <h1>Next Great Meal</h1>
+                </div> */}
+                <div className={style.flexi}>
+                    <div className={style.test}>
+                        <h1>Search from <span>Thousands of Recipes</span> for your <span>Next Great Meal</span></h1>
+                    </div>
+                    <div className={style.empty}>
+                        {/* <h1>asdf</h1> */}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
